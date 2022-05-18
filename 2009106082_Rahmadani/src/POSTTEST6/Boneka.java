9eafca6772cd68fa785d69ea09e868036c671349
Rahmadanii <rrahmadani900@gmail.com>
2022-05-19 00:04:27 +0800

    POSTTEST6

--- 2009106082_Rahmadani/src/POSTTEST6/Boneka.java
+++ 2009106082_Rahmadani/src/POSTTEST6/Boneka.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package POSTTEST6;
 
-/**
- *
- * @author Asus
- */
-public class Boneka {
+public abstract class Boneka {
+    protected String nama, ukuran, warna;
+    protected final int ukuranS = 25; //cm
+    protected final int ukuranM = 30; //cm
+    protected final int ukuranL = 60; //cm
+    protected final int ukuranXL = 80; //cm
     
+    // abstract method
+    public abstract void empuk();
+
+    public Boneka(String nama, String ukuran, String warna) {
+        this.nama = nama;
+        this.ukuran = ukuran;
+        this.warna = warna;
+    }
+
+    final public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
 }
